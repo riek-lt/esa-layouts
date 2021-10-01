8bac25ab31fd44e85598f430379ee432c08aa9e2
duncte123 <contact@duncte123.me>
2021-10-01 16:14:56 +0200

    Add some css

--- src/dashboard/checklist/main.vue
+++ src/dashboard/checklist/main.vue
@@ -6,8 +6,11 @@
       Uncheck all
     </v-btn>
     <h2>Checklist</h2>
-    <ul>
-      <li v-for="(check, i) in checks" :key="i">
+    <ul class="border">
+      <li :class="{
+        multiselect: true,
+        'multiselect-on': check.checked,
+      }" v-for="(check, i) in checks" :key="i">
         <input :id="`checkbox-${i}`" type="checkbox" v-model="check.checked">
         <label :for="`checkbox-${i}`">{{ check.title }}</label>
       </li>
@@ -26,31 +29,47 @@ export default class extends Vue {
       checked: false,
     },
     {
-      title: 'Play ads on BSG Channel',
-      checked: false,
-    },
-    {
       title: 'Press Next Game in Run Player',
       checked: false,
     },
     {
-      title: '',
+      title: 'Tweet about the run WITH a picture',
       checked: false,
     },
     {
-      title: '',
+      title: 'Balance audio with donation reader',
       checked: false,
     },
     {
-      title: '',
+      title: 'Write down end time of previous run (when no scheduler is active)',
       checked: false,
     },
     {
-      title: '',
+      title: 'Set Host and Commentators',
       checked: false,
     },
     {
-      title: '',
+      title: 'Verify Game Layout',
+      checked: false,
+    },
+    {
+      title: 'Verify run-info is correct',
+      checked: false,
+    },
+    {
+      title: 'Make sure stream(s) are loaded',
+      checked: false,
+    },
+    {
+      title: 'Check if online notifications are off',
+      checked: false,
+    },
+    {
+      title: 'Stay hydrated',
+      checked: false,
+    },
+    {
+      title: 'Final Check',
       checked: false,
     },
   ];
@@ -62,3 +81,28 @@ export default class extends Vue {
   }
 }
 </script>
+<style>
+  .border {
+    border: 2px solid black;
+    border-radius: 5px;
+    padding: 5px;
+  }
+
+  .multiselect {
+    border: solid 1px #768948;
+    overflow: auto;
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  .multiselect-on {
+    color: #ffffff;
+    background-color: #06BA63;
+  }
+
+  ul {
+    display: flex;
+    list-style-type: none;
+    flex-wrap: wrap;
+  }
+</style>
