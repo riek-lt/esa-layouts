9c765ec2dc9aea7ab56377ee3487c1812099dd90
duncte123 <contact@duncte123.me>
2022-10-05 12:59:22 +0200

    Assume only one index can be changed at a time

--- src/dashboard/video-matrix/main.vue
+++ src/dashboard/video-matrix/main.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-simple-table>
-      <template #default> <!-- v-slot:default -->
+      <template #default>
         <thead>
           <tr>
             <th class="text-left text-muted input-dash-output">
@@ -89,8 +89,8 @@ export default class extends Vue {
       return;
     }
 
-    // find the index that changed
-    const changedIndexes: number[] = [];
+    // find the index that changed, there's no way more than one index changes at once
+    let captureIndex = -1;
 
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < newVal.length; i++) {
@@ -98,23 +98,26 @@ export default class extends Vue {
       const oldNum = oldVal[i];
 
       if (newNum !== oldNum) {
-        changedIndexes.push(i);
+        captureIndex = i;
+        break;
       }
+    }
+
+    // Sanity check
+    if (captureIndex === -1) {
+      return;
     }
 
     this.locked = true;
 
-    for (const captureIndex of changedIndexes) {
-      const captureName = this.gameCaptures[captureIndex];
-      const sourceName = this.gameSources[newVal[captureIndex]];
+    const captureName = this.gameCaptures[captureIndex];
+    const sourceName = this.gameSources[newVal[captureIndex]];
 
-      // We're using a different event here to prevent loops.
-      // eslint-disable-next-line no-await-in-loop
-      await nodecg.sendMessage('setSelectedCaptures', {
-        sceneName: captureName,
-        sourceName,
-      });
-    }
+    // We're using a different event here to prevent loops.
+    await nodecg.sendMessage('setSelectedCaptures', {
+      sceneName: captureName,
+      sourceName,
+    });
 
     this.locked = false;
   }
