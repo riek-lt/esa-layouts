2723128599c904b606c79502ff076da542db283c
duncte123 <contact@duncte123.me>
2023-03-07 08:56:19 +0100

    Disable for in-person events

--- src/dashboard/restream-viewer-tool/main.vue
+++ src/dashboard/restream-viewer-tool/main.vue
@@ -1,13 +1,17 @@
 <template>
-  <v-app>
+  <v-app v-if="online">
     <RTMPFeed />
     <hr>
     <v-btn>Save</v-btn>
+  </v-app>
+  <v-app v-else>
+    <p>Disabled for in-person events</p>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { Configschema } from '@esa-layouts/types/schemas';
 import RTMPFeed from './components/RTMPFeed.vue';
 
 @Component({
@@ -15,7 +19,9 @@ import RTMPFeed from './components/RTMPFeed.vue';
     RTMPFeed,
   },
 })
-export default class extends Vue {}
+export default class extends Vue {
+  online = (nodecg.bundleConfig as Configschema).event.online;
+}
 </script>
 
 <style>
