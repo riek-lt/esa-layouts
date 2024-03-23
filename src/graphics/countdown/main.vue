6402ce53a8a5bc415911d246d99c55815455f6a3
duncte123 <contact@duncte123.me>
2024-03-23 15:59:23 +0100

    remove windows 10 update screen

--- src/graphics/countdown/main.vue
+++ src/graphics/countdown/main.vue
@@ -15,16 +15,6 @@
         <music-track hide-icon class="music" />
       </div>
     </div>
-
-    <div class="Flex winOverlay" :style="{
-      opacity: remaining > 0 ? '0' : '1',
-    }">
-      <div class="FlexColumn overlayInner">
-        <p>Event Starts Soon</p>
-        <div ref="spinner" class="spinner" />
-        <p>Don't turn off your computer</p>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -44,52 +34,12 @@ import { getZoomAmountCSS } from '../_misc/helpers';
 })
 export default class extends Vue {
   @State countdown!: CountdownType;
-  @Ref('spinner') spinner!: HTMLElement;
 
   zoom = getZoomAmountCSS();
-
-  get remaining() {
-    return this.countdown?.remaining ?? 0;
-  }
-
-  created(): void {
-    // let frame = 0xE100;
-    let frame = 0xE052;
-    const animFn = () => {
-      // eslint-disable-next-line no-plusplus
-      this.spinner.innerHTML = String.fromCharCode(frame++);
-
-      // if (frame >= 0xE176) {
-      //   frame = 0xE100;
-      // }
-
-      if (frame >= 0xE0CB) {
-        frame = 0xE052;
-      }
-    };
-
-    let last = performance.now();
-
-    const tick = () => {
-      if (performance.now() - last >= 22) {
-        animFn();
-
-        last = performance.now();
-      }
-      requestAnimationFrame(tick);
-    };
-
-    window.requestAnimationFrame(tick);
-  }
 }
 </script>
 
 <style lang="scss">
-@font-face {
-  font-family: windows;
-  src: url(./segoe_slboot.ttf) format('truetype');
-}
-
 #Countdown {
   position: absolute;
   top: 0;
@@ -102,34 +52,6 @@ export default class extends Vue {
 
     div div:last-child {
       margin-top: 0 !important;
-    }
-  }
-}
-
-.winOverlay {
-  opacity: 0;
-  background: #00a2ed;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 10000;
-  justify-content: center;
-  align-items: center;
-  transition: opacity 0.5s linear;
-
-  .overlayInner {
-    color: white;
-    text-align: center;
-    font-size: 3em;
-    display: inline-flex;
-    justify-self: center;
-    align-content: center;
-
-    .spinner {
-      font-family: windows;
-      font-size: 1.5em;
     }
   }
 }
