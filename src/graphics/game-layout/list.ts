feb503c4cbda175b30eefbd06010d19a124332d7
duncte123 <contact@duncte123.me>
2024-04-17 08:29:51 +0200

    4x3-2p

--- src/graphics/game-layout/list.ts
+++ src/graphics/game-layout/list.ts
@@ -15,7 +15,6 @@ import L_3DS_2p from './3ds-2p.vue';
 import L_4x3_1p from './4x3-1p.vue';
 import L_4x3_2p from './4x3-2p.vue';
 import L_4x3_3p from './4x3-3p.vue';
-import L_4x3_4p from './4x3-4p.vue';
 import L_DS_1p from './ds-1p.vue';
 import L_DSV_1p from './dsv-1p.vue';
 import L_DS_2p from './ds-2p.vue';
@@ -25,5 +24,5 @@ import L_GBA_1p from './gba-1p.vue';
 import L_GBA_2p from './gba-2p.vue';
 import L_FullCam from './full-cam.vue';
 
-export { L_4x3_1p, L_4x3_2p, L_4x3_3p, L_4x3_4p, L_16x9_1p, L_16x9_2p, L_16x9_2p_c, L_16x9_3p, L_3DS_1p, L_DS_1p, L_GB_1p, L_GBA_1p, L_16x9_1p_LargeCam, L_4x3_1p_LargeCam, L_3DSV_1p, L_3DS_2p, L_DS_2p, L_DSV_1p, L_GB_2p, L_GBA_2p, L_9x16_1p_2Cams, L_FullCam };
+export { L_4x3_1p, L_4x3_2p, L_4x3_3p, L_16x9_1p, L_16x9_2p, L_16x9_2p_c, L_16x9_3p, L_3DS_1p, L_DS_1p, L_GB_1p, L_GBA_1p, L_16x9_1p_LargeCam, L_4x3_1p_LargeCam, L_3DSV_1p, L_3DS_2p, L_DS_2p, L_DSV_1p, L_GB_2p, L_GBA_2p, L_9x16_1p_2Cams, L_FullCam };
 export const defaultCode = '4x3-1p';
